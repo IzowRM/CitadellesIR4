dc17187d4f886741dc9b5ce7ffb2e7e16d7a6efe
Robin Mougne <94527627+IzowRM@users.noreply.github.com>
2023-12-14 10:17:12 +0200

    Update Voleur.java

--- src/main/java/modele/Voleur.java
+++ src/main/java/modele/Voleur.java
@@ -1,8 +1,10 @@
 package modele;
 
+import java.util.Random;
+
 import controleur.Interaction;
 
-public class Voleur extends Personnage{
+public class Voleur extends Personnage {
     public Voleur() {
         super("Voleur", 2, Caracteristiques.VOLEUR);
 
@@ -12,24 +14,47 @@ public class Voleur extends Personnage{
     public void utiliserPouvoir() {
         boolean continu = true;
 
-        // pose la question à l'utilisateur
+        // pose la question ï¿½ l'utilisateur
         System.out.println("Quel personnage voulez-vous voler ?");
-        // récupère le choix de l'utilisateur
+        // rï¿½cupï¿½re le choix de l'utilisateur
         super.listePersonnagesDisponibles();
 
         do {
-            int  choix = Interaction.lireUnEntier(1, this.getPlateau().getNombrePersonnages(), "Votre choix entre: ");
+            int choix = Interaction.lireUnEntier(1, this.getPlateau().getNombrePersonnages(), "Votre choix entre: ");
             if (this.getPlateau().getPersonnage(choix - 1).equals(this)) {
-                System.out.println("Vous ne pouvez pas vous voler vous même");
+                System.out.println("Vous ne pouvez pas vous voler vous mï¿½me");
 
             } else {
-                int pieces = this.getPlateau().getPersonnage(choix-1).getJoueur().nbPieces();
-                this.getPlateau().getPersonnage(choix - 1).setVole();
-                this.getPlateau().getPersonnage(choix-1).getJoueur().retirerPieces(pieces);
-                this.getJoueur().ajouterPieces(pieces);
+                if (this.getPlateau().getPersonnage(choix - 1).getJoueur() != null) {
+                    this.getPlateau().getPersonnage(choix - 1).setVole();
+                }
                 continu = false;
             }
         } while (continu);
     }
 
+    public void utiliserPouvoirAvatar() {
+        boolean continu = true;
+        // pose la question ï¿½ l'utilisateur
+        System.out.println("Le voleur va voler");
+        // rï¿½cupï¿½re le choix de l'utilisateur
+        super.listePersonnagesDisponibles();
+
+        int choix = new Random().nextInt(this.getPlateau().getNombrePersonnages()) + 1;
+        do {
+            if (this.getPlateau().getPersonnage(choix - 1).equals(this)) {
+                choix++;
+                if (choix > this.getPlateau().getNombrePersonnages()) {
+                    choix = 1;
+                }
+            } else {
+                if (this.getPlateau().getPersonnage(choix - 1).getJoueur() != null) {
+                    this.getPlateau().getPersonnage(choix - 1).setVole();
+                    continu = false;
+                } else {
+                    continu = false;
+                }
+            }
+        } while (continu);
+    }
 }
