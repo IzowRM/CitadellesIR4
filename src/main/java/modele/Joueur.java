4b2b12f4ea02324f293d60cdb6b9867f9e434bdf
Robin Mougne <94527627+IzowRM@users.noreply.github.com>
2023-12-14 10:13:13 +0200

    Update Joueur.java

--- src/main/java/modele/Joueur.java
+++ src/main/java/modele/Joueur.java
@@ -1,7 +1,10 @@
 package modele;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
+import static modele.Quartier.TYPE_QUARTIERS;
 
 public class Joueur {
     private String nom;
@@ -12,6 +15,7 @@ public class Joueur {
     private ArrayList<Quartier> main;
     protected Personnage monPersonnage;
     private boolean isOrdinateur;
+    private boolean isFirst;
 
     public Joueur(String nom) {
         this.nom = nom;
@@ -22,16 +26,27 @@ public class Joueur {
         this.main = new ArrayList<Quartier>();
         this.monPersonnage = null;
         this.isOrdinateur = false;
+        this.isFirst = false;
+    }
+
+    public boolean isFirst() {
+        return isFirst;
+    }
+
+    public void setFirst(boolean first) {
+        isFirst = first;
     }
 
     public void setIsOrdinateur() {
         this.isOrdinateur = true;
     }
+
     public boolean getIsOrdinateur() {
         return this.isOrdinateur;
     }
+
     public String getNom() {
-        return nom;
+        return toString();
     }
 
 
@@ -83,13 +98,14 @@ public class Joueur {
 
     public void ajouterQuartierDansCite(Quartier quartier1) {
 
-        if (nbQuartiers < 8 && quartier1.getCout()<=tresor) {
+        if (nbQuartiers < 8 && quartier1.getCout() <= tresor) {
             cite[nbQuartiers] = quartier1;
             nbQuartiers++;
-        } else if (quartier1.getCout()>tresor){
+        } else if (quartier1.getCout() > tresor) {
             System.out.println("Vous n'avez pas les fonds pour construire ce quartier.");
         }
     }
+
     public boolean quartierPresentDansCite(String s) {
         for (int i = 0; i < nbQuartiers; i++) {
             if (cite[i] != null && cite[i].getNom().equals(s)) {
@@ -108,42 +124,49 @@ public class Joueur {
                     Quartier q1 = cite[i];
                     cite[i] = null;
                     return q1;
-
                 }
             }
 
         }
         return null;
     }
-    public void piocherUnQuartier(Pioche p){
-       ajouterQuartierDansMain( p.piocher());
+
+    public void piocherUnQuartier(Pioche p) {
+        ajouterQuartierDansMain(p.piocher());
     }
+
     public void ajouterQuartierDansMain(Quartier quartier1) {
         main.add(quartier1);
     }
-    public Quartier retirerUnQuartierDansMain(int n){
+
+    public Quartier retirerUnQuartierDansMain(int n) {
         Quartier q;
         q = main.get(n);
-       this.main.remove(q);
+        this.main.remove(q);
         return q;
     }
+
     public Quartier retirerQuartierDansMain() {
-        Quartier q =new Quartier();
+        Quartier q = new Quartier();
         Random generateur = new Random();
         int numeroHasard = generateur.nextInt(this.nbQuartiersDansMain());
         if (main.size() > 0) {
-            q=main.get(numeroHasard);
+            q = main.get(numeroHasard);
             main.remove(numeroHasard);
             return q;
         }
         return null;
     }
-    public Personnage getPersonnage(){
+
+    public Personnage getPersonnage() {
         return monPersonnage;
     }
-    public void setPersonnage(Personnage p){
+
+    public void setPersonnage(Personnage p) {
         monPersonnage = p;
+
     }
+
     public void reinitialiser() {
         tresor = 0;
         main.clear();
@@ -152,6 +175,44 @@ public class Joueur {
         }
     }
 
+    @Override
+    public String toString() {
+        return nom;
+    }
 
+    public int calculerPoints() {
+        int totalPoints = 0;
 
+        for (int i = 0; i < nbQuartiers; i++) {
+            totalPoints += cite[i].getCout();
+        }
+
+        if (aAuMoinsUnDeChaqueType()) {
+            totalPoints += 3;
+        }
+
+        if (isFirst) {
+            totalPoints += 4;
+        }
+
+        if (nbQuartiers == 8 && !isFirst) {
+            totalPoints += 2;
+        }
+
+        return totalPoints;
+    }
+
+    private boolean aAuMoinsUnDeChaqueType() {
+
+        List<String> typesPresent = new ArrayList<>();
+
+        for (int i = 0; i < nbQuartiers; i++) {
+            String type = cite[i].getType();
+            if (!typesPresent.contains(type)) {
+                typesPresent.add(type);
+            }
+        }
+
+        return typesPresent.size() == TYPE_QUARTIERS.length;
+    }
 }
