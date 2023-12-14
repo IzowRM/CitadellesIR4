21223e58ded9a0b03da901ff5d953eb1c9bd2146
Robin Mougne <94527627+IzowRM@users.noreply.github.com>
2023-12-14 10:09:36 +0200

    Update Interaction.java

--- src/main/java/controleur/Interaction.java
+++ src/main/java/controleur/Interaction.java
@@ -26,26 +26,25 @@ public class Interaction {
 
     // renvoie un entier lu au clavier compris dans l'intervalle
     //     [borneMin, borneMax]
-    //     modification des variables d'entrée pour plus de flexibilité.
-    public static int lireUnEntier(int borneMin, int borneMax,String msg) {
+    //     modification des variables d'entrÃ©e pour plus de flexibilitÃ©.
+    public static int lireUnEntier(int borneMin, int borneMax, String msg) {
         int i = 0;
         boolean continu = true;
         do {
             try {
-                System.out.print(msg+" "+ borneMin + " et " + borneMax + " : ");
+                System.out.print(msg + " " + borneMin + " et " + borneMax + " : ");
                 i = sc.nextInt();
+                sc.nextLine();  // Consomme la nouvelle ligne restante
                 if (i >= borneMin && i <= borneMax) {
-                    continu = false; // L'entier est dans la plage spécifiée, la boucle s'arrête
+                    continu = false;
                 } else {
-                    System.out.println("L'entier doit être compris entre " + borneMin + " et " + borneMax + ".");
-                    continu = true; // L'entier n'est pas dans la plage, la boucle continue
+                    System.out.println("L'entier doit Ãªtre compris entre " + borneMin + " et " + borneMax + ".");
+                    continu = true;
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Ce n'est pas un entier valide.");
-                sc.next(); // Passe l'entrée incorrecte pour éviter de boucler
-                continu = true; // La boucle continue
-            }finally {
-                sc.nextLine(); // consommer le caractère de nouvelle ligne
+                sc.nextLine();  // Consomme la nouvelle ligne incorrecte
+                continu = true;
             }
         } while (continu);
         return i;
@@ -57,20 +56,21 @@ public class Interaction {
 
         while (true) {
             try {
-                System.out.print("Veuillez répondre par 'oui' (o) ou 'non' (n) : ");
+                System.out.print("Veuillez rÃ©pondre par 'oui' (o) ou 'non' (n) : ");
+
                 String choix = sc.nextLine().toLowerCase();
 
                 if (choix.equals("oui") || choix.equals("o")) {
                     retour = true;
-                    break; // Sortir de la boucle si la réponse est valide
+                    break; // Sortir de la boucle si la rÃ©ponse est valide
                 } else if (choix.equals("non") || choix.equals("n")) {
                     retour = false;
-                    break; // Sortir de la boucle si la réponse est valide
+                    break; // Sortir de la boucle si la rÃ©ponse est valide
                 } else {
-                    throw new OuiNonException("Réponse invalide. Veuillez répondre par 'oui' (o) ou 'non' (n).");
+                    throw new OuiNonException("RÃ©ponse invalide. Veuillez rÃ©pondre par 'oui' (o) ou 'non' (n).");
                 }
             } catch (OuiNonException e) {
-                System.out.println(e.getMessage()); // Afficher le message d'erreur personnalisé
+                System.out.println(e.getMessage()); // Afficher le message d'erreur personnalisÃ©
             }
         }
 
@@ -87,7 +87,7 @@ public class Interaction {
     public static int returnInt(int i){
         Random random = new Random();
 
-        // Générer un chiffre aléatoire entre 0 (inclus) et 4 (exclus)
+        // GÃ©nÃ©rer un chiffre alÃ©atoire entre 0 (inclus) et 4 (exclus)
         return random.nextInt(i);
     }
 
